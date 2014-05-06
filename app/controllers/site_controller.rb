86f046cbd0844a31504dc49cfe51c369e80c5224
simon245 <simonball245@gmail.com>
2014-05-06 17:49:41 +0800

    ADDED Calendar to index page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,9 @@
 class SiteController < ApplicationController
 
   def index
-
+    @events = Events.all
+    @events_by_date = @events.group_by(&:event_date)
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
 
   def event
